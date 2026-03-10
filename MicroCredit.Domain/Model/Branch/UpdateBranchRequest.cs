d740f91f4ae6b79c8a41cf52584e1ceea5409f78
aneeshchandra999 <aneeshchandra999@gmail.com>
2026-03-10 23:09:35 +0530

    Fixed Update issue

--- MicroCredit.Domain/Model/Branch/UpdateBranchRequest.cs
+++ MicroCredit.Domain/Model/Branch/UpdateBranchRequest.cs
@@ -8,13 +8,13 @@ namespace MicroCredit.Domain.Model.Branch
 {
     public class UpdateBranchRequest
     {
-        public string Name { get; private set; } = string.Empty;
-        public string Address1 { get; private set; } = string.Empty;
-        public string Address2 { get; private set; } = string.Empty;
-        public string City { get; private set; } = string.Empty;
-        public string State { get; private set; } = string.Empty;
-        public string Country { get; private set; } = string.Empty;
-        public string ZipCode { get; private set; } = string.Empty;
-        public string PhoneNumber { get; private set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Address1 { get;  set; } = string.Empty;
+        public string Address2 { get;  set; } = string.Empty;
+        public string City { get;  set; } = string.Empty;
+        public string State { get;  set; } = string.Empty;
+        public string Country { get;  set; } = string.Empty;
+        public string ZipCode { get;  set; } = string.Empty;
+        public string PhoneNumber { get;  set; } = string.Empty;
     }
 }
